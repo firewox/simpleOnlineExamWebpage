82924a6c21dd8ff5bc497b67f936498b430c3002
jimmy <lyutian2020@qq.com>
2022-01-22 14:19:53 +0800

    将web_myJavaWork工程添加到本地仓库

---
+++ web_myJavaWork/src/main/java/cn/tust/myJavaWork/Hello.java
@@ -0,0 +1,11 @@
+package cn.tust.myJavaWork;
+
+/**
+ * @Author: Liyutian
+ * @Date:Create：in 2022/1/22 11:29
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
